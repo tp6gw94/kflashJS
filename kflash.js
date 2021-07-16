73704f0503c30dd427d34fafa5af4c81ef935b1a
Rick <lyf92013@gmail.com>
2021-07-16 11:47:36 +0800

    add simple firmware

--- kflash.js
+++ kflash.js
@@ -422,7 +422,7 @@ class KFlash {
             // console.log(packet);
             // console.log(packet.map((e) => e.toString(16)));
             console.log("write", `0x${address.toString(16)}`, packet.length);
-            this.write(packet);
+            await this.write(packet);
             if (await this.recv_debug()) {
               address += DATAFRAME_SIZE;
               break;
@@ -490,7 +490,7 @@ class KFlash {
         let reason = 0;
 
         while (true) {
-          this.write(packet);
+          await this.write(packet);
           retry_count++;
           try {
             const result = FlashModeResponse.parse(
@@ -555,7 +555,7 @@ class KFlash {
             // console.log(packet);
             // console.log(packet.map((e) => e.toString(16)));
             console.log("write", `0x${address.toString(16)}`);
-            this.write(packet);
+            await this.write(packet);
             if (await this.recv_debug()) {
               address += ISP_FLASH_DATA_FRAME_SIZE;
               break;
